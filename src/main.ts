1ddb68fef3f5b474f43bb881d709d6b6fe790c71
Leonardo Yoshii <leonardo.yoshii@objective.com.br>
2022-06-15 22:27:42 -0300

    Commit Initial

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { Logger } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+const port = 3000;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Country Challenge Example')
+    .setDescription('Country Challenge API Description')
+    .setVersion('1.0')
+    .addTag('CountryChallenge')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('challenge', app, document);
+
+  await app.listen(port);
+}
+bootstrap();
